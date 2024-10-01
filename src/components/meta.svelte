db0ce8f321fecf39216ced1a0f6e458fd2a25e05
Jaren Goldberg <fyrlex@gmail.com>
2024-10-01 10:19:19 -0400

    fix: meta

--- src/components/meta.svelte
+++ src/components/meta.svelte
@@ -10,19 +10,13 @@
 <meta name="viewport" content="width=device-width, initial-scale=1" />
 <meta
 	name="description"
-	content="D & Z Motorsports services and repairs all makes and models of vehicles."
+	content="In need of an auto repair or upgrade? At D & Z Motorsports, we are committed to providing a
+				fast and effective environment for automobiles and their needs."
 />
-<!-- <link
-	rel="shortcut icon"
-	href="https://cdn.discordapp.com/avatars/292821168833036288/3284fe8baadfec0fa38834643ff8a8f5.png"
-	type="image/png"
-/> -->
+<link rel="shortcut icon" href="https://dandzmotorsports.com/favicon.png" type="image/png" />
 <meta property="og:title" content={data.title} />
 <meta property="og:description" content="Welcome to D & Z Motorsports." />
 <meta property="og:type" content="website" />
 <meta property="og:url" content="https://dandzmotorsports.com/{data.url}" />
-<!-- <meta
-	property="og:image"
-	content="https://cdn.discordapp.com/avatars/292821168833036288/3284fe8baadfec0fa38834643ff8a8f5.png"
-/> -->
+<meta property="og:image" content="https://dandzmotorsports.com/favicon.png" />
 <meta content="#AD0302" data-react-helmet="true" name="theme-color" />
